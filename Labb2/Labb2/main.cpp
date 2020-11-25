8cc987d6c92f34955397752d0915ac9ea4ec7d9f
keppoloco <70692551+keppoloco@users.noreply.github.com>
2020-11-25 19:48:18 +0100

    fixed GetData function

--- Labb2/Labb2/main.cpp
+++ Labb2/Labb2/main.cpp
@@ -3,21 +3,40 @@
 #include "RoundedRectangle.h"
 #include <vector>
 #include <iostream>
+#include <numeric>
+#include <time.h>
+
+void GetData(const std::vector<Shape*> &arr)
+{
+	double area = 0;
+	std::cout << "Individual area and colors of shapes: " << '\n';
+	for (const auto& a : arr) {
+		std::cout << a->GetArea() << " -----> " << a->GetColour() << '\n';
+		area += a->GetArea();
+	}
+	std::cout << '\n';
+	std::cout << "Total area: " << area << '\n';
+}
 
 int main()
 {
-	Circle circle_(2);
+	std::vector<Shape*> shape_arr;
 
-	Cylinder Cylinder_(3, 2);
+	shape_arr = {
+		new Circle(3.2),
+		new Cylinder(3.5,4),
+		new Rectangle(4,5),
+		new parallelepiped(4,5,6),
+		new RoundedRectangle(6,5,3.2)
+	};
+	shape_arr[0]->setColour("Turqouise");
+	shape_arr[1]->setColour("Beige");
+	shape_arr[2]->setColour("Purple");
+	shape_arr[3]->setColour("Gray");
+	shape_arr[4]->setColour("Green");
 
-	Rectangle Rectangle_(3, 4);
 
-	parallelepiped Parallelepiped_(5, 4, 2);
-
-	RoundedRectangle RoundedRectangle_(6, 3, 2);
-
-
-	
+	GetData(shape_arr);
 
 	return 0;
 }
